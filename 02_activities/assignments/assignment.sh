9f8c6110c00d539948c790e01540711c08a9d07a
Mary Randle <maryrandle19@gmail.com>
2025-11-07 12:37:41 -0500

    complete 1h assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,7 +43,10 @@ cp ./data/raw/*event*log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm -f ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-Get-ChildItem -Path ./data/processed -Recurse -File | Select-Object -ExpandProperty FullName | Out-File ./data/inventory.txt
+New-Item -ItemType Directory -Force -Path ./data | Out-Null
+Get-ChildItem -Path ./data/processed -Recurse -File |
+  Select-Object -ExpandProperty FullName |
+  Set-Content -Path ./data/inventory.txt
 
 ###########################################
 
