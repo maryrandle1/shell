01222590e0e44dd91c512a3dca40ae20caf40907
Mary Randle <maryrandle19@gmail.com>
2025-11-07 15:02:21 -0500

    complete assignment 1h

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -21,7 +21,7 @@ touch README.md
 touch analysis/main.py
 
 # download client data
-curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+curl --ssl-no-revoke -LO https://github.com/UofT-DSI/shell/raw/main/02_activities/assignments/rawdata.zip
 unzip -q rawdata.zip
 
 ###########################################
@@ -43,9 +43,8 @@ cp ./data/raw/*event*log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm -f ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-New-Item -ItemType Directory -Path ./data -Force | Out-Null
-New-Item -ItemType File -Path ./data/inventory.txt -Force | Out-Null
-Get-ChildItem -Path ./data/processed -Recurse -File
+ls -R ./data/processed > ./data/inventory.txt
+
 
 ###########################################
 
